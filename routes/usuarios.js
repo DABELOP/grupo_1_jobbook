c5ea00211db26b23946278d0216018a2d09905dc
MarceMadrid <marcemadrid1995@gmail.com>
2022-10-18 21:54:01 -0500

    Se avanzo en la edicion del profile

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -8,7 +8,9 @@ router.get('/register', usuariosController.register);
 router.get('/:id/contacto/:idServicio', usuariosController.contacto);
 router.get('/profile/:id', usuariosController.profile);
 router.get('/profile/:id/servicios', usuariosController.misServicios);
-
+//Modificar un perfil
+router.get('/profile/editar/:id', usuariosController.editar);
+router.put('/profile/editar/:id', upload.any(), usuariosController.guardarEdicion);
 
 
 
