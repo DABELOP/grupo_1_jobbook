c7def61395813d55c9e0378256e2d87173e564a4
MarceMadrid <marcemadrid1995@gmail.com>
2022-10-26 17:19:56 -0500

    se completo el formulario de edicion de perfil

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -22,6 +22,7 @@ router.get('/register', usuariosController.register);
 router.get('/:id/contacto/:idServicio', usuariosController.contacto);
 router.get('/profile/:id', usuariosController.profile);
 router.get('/profile/:id/servicios', usuariosController.misServicios);
+
 //Modificar un perfil
 router.get('/profile/editar/:id', usuariosController.editar);
 router.put('/profile/editar/:id', upload.any(), usuariosController.guardarEdicion);
