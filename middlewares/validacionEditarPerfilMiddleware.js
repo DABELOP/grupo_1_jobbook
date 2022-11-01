c144ee9d11062751637748175198b4d6d9d399a2
Danilo Bedoya <danilobedoya94@gmail.com>
2022-10-31 20:42:54 -0500

    Co-authored-by: Juan Guillermo Morales <jugui93@users.noreply.github.com> Co-authored-by: MarceMadrid <MarceMadrid@users.noreply.github.com>

--- middlewares/validacionEditarPerfilMiddleware.js
+++ middlewares/validacionEditarPerfilMiddleware.js
@@ -2,13 +2,13 @@ const {body} = require("express-validator");
 
 module.exports= [
     body("nombre").notEmpty().withMessage("Debes escribir tu nombre"),
-    body("correo").isEmail().withMessage("Debes escribir tu correo"),
     body("nombrePersonalizado").notEmpty().withMessage("Debes escribir tu nombre personalizado"),
     body("password").isLength({min:8}).withMessage("La contraseña debe tener minimo 8 caracteres"),
     body("numeroDocumento").isLength({min:4}).withMessage('Debes ingresar un número válido').bail()
     .isInt().withMessage('Debes de ingresar solo numeros, sin espacios y sin caracteres especiales'),
     body("celular").isLength({min:9}).withMessage('Debes ingresar un número válido').bail()
     .isInt().withMessage('Debes de ingresar solo numeros, sin espacios y sin caracteres especiales'),
-    body("ciudad").isAlpha().isLength({min:4}).withMessage("Debes ingresar una ciudad válida")
+    body("ciudad").isAlpha().withMessage("La ciudad no debe contener números")
+    .isLength({min:4}).withMessage("Debes ingresar una ciudad válida")
 ]
 
