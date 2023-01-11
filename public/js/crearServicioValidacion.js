cb995876c7473abf806a7768cd7fa5b5c3b8e790
= <=>
2023-01-10 23:27:26 -0500

    Validacion frontend form creacion de servicios

---
+++ public/js/crearServicioValidacion.js
@@ -0,0 +1,30 @@
+window.onload = () => {
+    let formulario = document.querySelector('form.formularioLogin');
+
+    formulario.titulo.focus();
+    formulario.onsubmit = (event) =>{
+        let errores = [];
+        if (formulario.titulo.value == ''){
+            errores.push('El campo título es obligatorio.')
+        };
+        if (formulario.titulo.value.length < 5 ){
+            errores.push('El campo título debe tener al menos 5 caracteres')
+        };
+        if (formulario.descripcion.value.length < 20 ){
+            errores.push('El campo descripción debe tener al menos 20 caracteres')
+        };
+        if (formulario.precio.value == ''){
+            errores.push('El campo precio es obligatorio.')
+        };
+        if (formulario.tarifa.value == ''){
+            errores.push('El campo tarifa es obligatorio.')
+        };
+
+
+        console.log(errores);
+        if (errores.length > 0){
+            event.preventDefault();
+        }
+        
+    }
+};
