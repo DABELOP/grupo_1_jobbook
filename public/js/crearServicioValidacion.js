d35f1c534d49bfdef0e6c4e83919dedd1197ff3f
= <=>
2023-01-12 23:33:04 -0500

    Mostrar errores validacion en pantalla

--- public/js/crearServicioValidacion.js
+++ public/js/crearServicioValidacion.js
@@ -1,30 +1,48 @@
 window.onload = () => {
     let formulario = document.querySelector('form.formularioLogin');
+    let imagenes = document.querySelectorAll('.imagen');
+    const extensionesPermitidas = /(.jpg|.jpeg|.png|.gif)$/i;
 
     formulario.titulo.focus();
-    formulario.onsubmit = (event) =>{
+    formulario.onsubmit = (event) => {
         let errores = [];
-        if (formulario.titulo.value == ''){
+        if (formulario.titulo.value == '') {
             errores.push('El campo título es obligatorio.')
         };
-        if (formulario.titulo.value.length < 5 ){
+        if (formulario.titulo.value.length < 5) {
             errores.push('El campo título debe tener al menos 5 caracteres')
         };
-        if (formulario.descripcion.value.length < 20 ){
+        if (formulario.descripcion.value.length < 20) {
             errores.push('El campo descripción debe tener al menos 20 caracteres')
         };
-        if (formulario.precio.value == ''){
+        if (formulario.precio.value == '') {
             errores.push('El campo precio es obligatorio.')
         };
-        if (formulario.tarifa.value == ''){
+        if (formulario.tarifa.value == '') {
             errores.push('El campo tarifa es obligatorio.')
         };
 
+        for (let i = 0; i < imagenes.length; i++) {
+            if (imagenes[i].value == '') {
+            
+            } else if (!extensionesPermitidas.exec(imagenes[i].value)) {
+                errores.push('Las extensiones permitidas para imagenes son .jpg, .jpeg, .png, .gif');
+                break
+            }
+        };
 
         console.log(errores);
-        if (errores.length > 0){
+        if (errores.length > 0) {
             event.preventDefault();
         }
-        
+
+        let ulErrores = document.querySelector(".errores ul");
+
+        while (ulErrores.hasChildNodes()) {
+            ulErrores.removeChild(ulErrores.firstChild);
+        };
+        errores.forEach(error => {
+            ulErrores.innerHTML += `<li>${error}</li>`
+        });
     }
-};
+}
